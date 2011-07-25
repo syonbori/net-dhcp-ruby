cd83c5db5297270452af5bd5bc52e61131015c1d
syonbori <syonbori+github@mail.syonbori.net>
2011-07-25 21:31:21 +0900

    change dir structure

--- lib/net/dhcp.rb
+++ lib/net/dhcp.rb
@@ -6,15 +6,15 @@
 **  - Daniel Martin Gomez <etd[-at-]nomejortu.com>
 **
 ** Desc:
-**   This package provides a set of classes to work with the DHCP protocol. They 
+**   This package provides a set of classes to work with the DHCP protocol. They
 ** provide low level access to all the fields and internals of the protocol.
-** 
+**
 ** See the provided rdoc comments for further information.
 **
 ** More information in:
 **   - rfc2131: Dynamic Host Configuration Protocol
 **   - rfc2132: DHCP Options and BOOTP Vendor Extensions
-**   - rfc2563: DHCP Option to Disable Stateless Auto-Configuration in 
+**   - rfc2563: DHCP Option to Disable Stateless Auto-Configuration in
 **              IPv4 Clients
 **   - rfc4578: DHCP Options for the Intel Preboot eXecution Environment (PXE)
 **   - rfc4702: The DHCP Client Fully Qualified Domain Name (FQDN) Option
@@ -39,10 +39,10 @@
 =end
 
 # constants defining the values in the different fields of the protocols
-require 'dhcp/constants'
+require 'net/dhcp/constants'
 
 # DHCP options as defined in rfc2132 and rfc2563
-require 'dhcp/options'
+require 'net/dhcp/options'
 
 # DHCP messages as defined in rfc2131
-require 'dhcp/core'
+require 'net/dhcp/core'
